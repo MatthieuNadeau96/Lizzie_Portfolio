c42d0b23e322671af0be9e5a1937bd627ffe7852
Matthieu <matthieunadeau96@gmail.com>
2020-11-21 10:11:54 -0500

    Added proper positioning for the HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,50 +9,74 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     var deviceSize = MediaQuery.of(context).size;
-    var theme = Theme.of(context);
+    ThemeData theme = Theme.of(context);
+    final desktop = deviceSize.width >= 770;
+
+    String mobileImage = 'assets/images';
+    String desktopImage = 'images/';
+    String image = desktop ? desktopImage : mobileImage;
+
     return Scaffold(
-      body: Container(
-        height: deviceSize.height,
-        color: theme.canvasColor,
-        child: Stack(
-          children: [
-            Align(
-              alignment: Alignment(0, -0.5),
-              child: Container(
-                height: deviceSize.height * 0.7,
-                width: deviceSize.width * 0.75,
-                padding: EdgeInsets.all(0),
-                child: Image(
-                  image: AssetImage('images/title-icon.png'),
-                  fit: BoxFit.contain,
+      body: Center(
+        child: Container(
+          height: deviceSize.height * 0.5,
+          // width: deviceSize.width * 0.75,
+          color: theme.canvasColor,
+          child: Stack(
+            alignment: Alignment.center,
+            fit: StackFit.expand,
+            children: [
+              Align(
+                alignment: Alignment(0, -0.75),
+                child: Container(
+                  height: deviceSize.height * 0.40,
+                  child: Image(
+                    image: AssetImage('$image/title-icon.png'),
+                    width: deviceSize.width,
+                    fit: BoxFit.contain,
+                  ),
                 ),
               ),
-            ),
-            Align(
-              alignment: Alignment(0, 0.5),
-              child: Container(
-                height: deviceSize.height * 0.25,
-                width: deviceSize.width * 0.75,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Image(
-                      image: AssetImage('images/about-me-icon.png'),
-                    ),
-                    Image(
-                      image: AssetImage('images/graphic-design-icon.png'),
-                    ),
-                    Image(
-                      image: AssetImage('images/website-icon.png'),
-                    ),
-                    Image(
-                      image: AssetImage('images/writing-icon.png'),
-                    ),
-                  ],
+              Align(
+                alignment: Alignment(0, 0.75),
+                child: Container(
+                  height: deviceSize.height * 0.15,
+                  width: deviceSize.width >= 1000
+                      ? deviceSize.width * 0.5
+                      : deviceSize.width,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Expanded(
+                        child: Image(
+                          image: AssetImage('$image/about-me-icon.png'),
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                      Expanded(
+                        child: Image(
+                          image: AssetImage('$image/graphic-design-icon.png'),
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                      Expanded(
+                        child: Image(
+                          image: AssetImage('$image/website-icon.png'),
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                      Expanded(
+                        child: Image(
+                          image: AssetImage('$image/writing-icon.png'),
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
