4bf5b3beab7be5faa5e40ecc42cb42ff12ff38f0
Matthieu <matthieunadeau96@gmail.com>
2020-11-26 11:06:33 -0500

    In the about screen I made the image scrollable and added fontawesome icons for the social links

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutScreen extends StatefulWidget {
   @override
@@ -6,6 +8,14 @@ class AboutScreen extends StatefulWidget {
 }
 
 class _AboutScreenState extends State<AboutScreen> {
+  void _handlePress(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     var deviceSize = MediaQuery.of(context).size;
@@ -21,6 +31,9 @@ class _AboutScreenState extends State<AboutScreen> {
             color: Colors.white,
             borderRadius: BorderRadius.circular(25),
           ),
+          margin: const EdgeInsets.only(
+            bottom: 56,
+          ),
           padding: const EdgeInsets.symmetric(
             horizontal: 30,
             vertical: 15,
@@ -28,24 +41,28 @@ class _AboutScreenState extends State<AboutScreen> {
           width: 500,
           child: Column(
             children: [
-              Center(
-                child: Container(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 15,
-                  ),
-                  child: Placeholder(
-                    fallbackHeight: 300,
-                    fallbackWidth: 250,
-                  ),
-                ),
-              ),
               Expanded(
                 child: SingleChildScrollView(
-                  child: Text(
-                    'I am a freelance artist and visual communicator. I started my own art brand, Good Dog Draws, in 2017 and love seeing how it has evolved and grown. Animals, plants, and shoes are my favorite drawing subjects. Procreate is my go-to software, but I used Adobe Suite all throughout college. As a quick and curious learner, I’m sure I will venture into other softwares at one point or another.\n\nI graduated from the University of Vermont with a B.S. in Public Communication and minored in Studio Art. I currently am accepting personal commissions (portraits, custom illustrations, etc), as well as professional business commissions (logos, signage, pamphlets, etc). I am also open to working with a single company full time if it is the right fit. Please do not hesitate to reach out and start a conversation about how I can be of assistance to you or your company.',
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
+                  child: Column(
+                    children: [
+                      Center(
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 15,
+                          ),
+                          child: Placeholder(
+                            fallbackHeight: 300,
+                            fallbackWidth: 250,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        'I am a freelance artist and visual communicator. I started my own art brand, Good Dog Draws, in 2017 and love seeing how it has evolved and grown. Animals, plants, and shoes are my favorite drawing subjects. Procreate is my go-to software, but I used Adobe Suite all throughout college. As a quick and curious learner, I’m sure I will venture into other softwares at one point or another.\n\nI graduated from the University of Vermont with a B.S. in Public Communication and minored in Studio Art. I currently am accepting personal commissions (portraits, custom illustrations, etc), as well as professional business commissions (logos, signage, pamphlets, etc). I am also open to working with a single company full time if it is the right fit. Please do not hesitate to reach out and start a conversation about how I can be of assistance to you or your company.',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
@@ -56,21 +73,26 @@ class _AboutScreenState extends State<AboutScreen> {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    Placeholder(
-                      fallbackHeight: 50,
-                      fallbackWidth: 50,
+                    IconButton(
+                      onPressed: () => _handlePress(
+                          'https://www.instagram.com/gooddoggyboy/'),
+                      icon: FaIcon(
+                        FontAwesomeIcons.instagram,
+                      ),
                     ),
-                    Placeholder(
-                      fallbackHeight: 50,
-                      fallbackWidth: 50,
+                    IconButton(
+                      onPressed: () => _handlePress(
+                          'https://www.linkedin.com/in/lizzie-clarke/'),
+                      icon: FaIcon(
+                        FontAwesomeIcons.linkedin,
+                      ),
                     ),
-                    Placeholder(
-                      fallbackHeight: 50,
-                      fallbackWidth: 50,
-                    ),
-                    Placeholder(
-                      fallbackHeight: 50,
-                      fallbackWidth: 50,
+                    IconButton(
+                      onPressed: () =>
+                          _handlePress('mailto:elizabeth.clarke.20@gmail.com'),
+                      icon: FaIcon(
+                        FontAwesomeIcons.envelope,
+                      ),
                     ),
                   ],
                 ),
