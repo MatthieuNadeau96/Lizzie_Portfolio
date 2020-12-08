29a04d903b828b6dca861bf35f4b579c676a0738
Matthieu <matthieunadeau96@gmail.com>
2020-12-08 17:04:02 -0500

    Populated AboutScreen with a new profile photo and a Medium social link

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -19,6 +20,9 @@ class _AboutScreenState extends State<AboutScreen> {
   @override
   Widget build(BuildContext context) {
     var deviceSize = MediaQuery.of(context).size;
+    String mobileImage = 'assets/images';
+    String desktopImage = 'images/';
+    String image = (kIsWeb) ? desktopImage : mobileImage;
     ThemeData theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
@@ -47,14 +51,12 @@ class _AboutScreenState extends State<AboutScreen> {
                     children: [
                       Center(
                         child: Container(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 15,
-                          ),
-                          child: Placeholder(
-                            fallbackHeight: 300,
-                            fallbackWidth: 250,
-                          ),
-                        ),
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 15,
+                            ),
+                            child: Image(
+                              image: AssetImage('$image/about-me-photo.png'),
+                            )),
                       ),
                       Text(
                         'I am a freelance artist and visual communicator. I started my own art brand, Good Dog Draws, in 2017 and love seeing how it has evolved and grown. Animals, plants, and shoes are my favorite drawing subjects. Procreate is my go-to software, but I used Adobe Suite all throughout college. As a quick and curious learner, I’m sure I will venture into other softwares at one point or another.\n\nI graduated from the University of Vermont with a B.S. in Public Communication and minored in Studio Art. I currently am accepting personal commissions (portraits, custom illustrations, etc), as well as professional business commissions (logos, signage, pamphlets, etc). I am also open to working with a single company full time if it is the right fit. Please do not hesitate to reach out and start a conversation about how I can be of assistance to you or your company.',
@@ -92,6 +94,13 @@ class _AboutScreenState extends State<AboutScreen> {
                           _handlePress('mailto:elizabeth.clarke.20@gmail.com'),
                       icon: FaIcon(
                         FontAwesomeIcons.envelope,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () =>
+                          _handlePress('https://elizabeth-clarke.medium.com'),
+                      icon: FaIcon(
+                        FontAwesomeIcons.medium,
                       ),
                     ),
                   ],
